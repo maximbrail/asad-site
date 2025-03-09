484e9379a056f92d0950ccebd9e31d65ce092ac5
maximbrail <maxabobus953@gmail.com>
2025-03-09 17:39:57 +0500

    Add files via upload

--- src/Page/Comparision.js
+++ src/Page/Comparision.js
@@ -7,7 +7,7 @@ export const Comparision = () => {
       <h1 className='comparison-title'>Comparison</h1>
       <h2 className='comparison-podtitle'>Enter your details to compare: </h2>
       <input type='text' placeholder='Type your name...' className='comparison-input'/>
-      <input type='number' placeholder='Type your height(in cm)...' className='comparison-input'/>
+      <input type='number' placeholder='Type your height(cm)' className='comparison-input'/>
       <select className='select-gen'>
         <option value="Male">Male</option>
         <option value="Female">Female</option>
