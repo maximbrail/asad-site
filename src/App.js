c34fc4d90ef740652abcf63df32af36734150f58
maximbrail <maxabobus953@gmail.com>
2025-03-09 17:35:07 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import "./index.css"
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import React from 'react'
 
-import { NavCom } from './Components/NavCom';
-import { HeaderCom } from './Components/HeaderCom';
-import { MainCom } from './Components/MainCom';
-import FooterCom from './Components/FooterCom';
 
+import { Home } from "./Page/Home";
+import { About } from "./Page/About";
+import { Contacts } from "./Page/Contacts";
+import { Service } from "./Page/Service";
+
+
+
+import { NavCom } from './Components/NavCom'
 
 
 function App() {
   return (
-    <Router className="App">
-      <NavCom />
-      <HeaderCom />
-      <MainCom />
-      <FooterCom />
+    <Router className='App'>
+      
+      <NavCom/>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/service" element={<Service/>}/>
+        <Route path="/contacts" element={<Contacts/>}/>
+      </Routes>
+
     </Router>
-
-
   );
 }
 
--- src/Components/NavCom.js
+++ src/Components/NavCom.js
@@ -1,27 +1,21 @@
 import React from 'react'
-import './styleCom.css'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import '../index.css'
 
 export const NavCom = () => {
   return (
-    <div className='nav'>
-        <div className='nav-logo'>
-        <img className='nav-logo-img' src='https://about-asad.netlify.app/static/media/logo.492c5bc4920befbf827c.png' alt='asad'/>
-        <h3 className='nav-logo-title'>Asad</h3>
-        </div>
+    <div className='navbar'>
+        <nav>
+            <div className='nav-logo'>
+                <Link to='/'>Asad</Link>
+            </div>
 
-        <div className='nav-links'>
-            <Link to="/">Home</Link>
-            <Link to="/works">Works</Link>
-            <Link to="/comparision">Comparision</Link>
-            <Link to="/skills">Skills</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-        </div>
-
+            <div className='nav-links'>
+                <Link to='/about'>About</Link>
+                <Link to='/service'>Service</Link>
+                <Link to='/contacts'>Contacts</Link>
+            </div>
+        </nav>
     </div>
   )
 }
-
-
-export default NavCom
